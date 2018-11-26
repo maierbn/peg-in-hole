1a4d772d6773a73e4abc2a3acf5966a1e68a8549
Knieboike <33316007+Knieboike@users.noreply.github.com>
2018-11-26 13:32:55 +0100

    Kleine Klassen und Standardwerte

--- peg-in-hole/src/userInterface/Main.java
+++ peg-in-hole/src/userInterface/Main.java
@@ -6,5 +6,27 @@ public class Main {
 		// TODO Auto-generated method stub
 
 	}
-
+	public void werte() {
+		float wx = 0;		//position
+		float wy = 0;		//position 
+		float t = 0; 		//time
+		float E = 0;		// Young's modulus (a quantity that measures an object or substance's resistance to being deformed elasticall)
+		float I = 0;		// 2nd Area Moment (Flächenträgheitsmoment)
+		float q = 0;		// body load (external force acting upon the beam)
+		int x= 0;			// FIX Point left side
+		float L = 0;		// other End of the Object where x = L
+		float wofx = 0;		//deflection w(x) of the beam
+	}
+	public float Inertia(float I) {
+		float h = 0;		//height
+		float b = 0;		//width
+		I = (b*(h*h*h))/12;
+		return I;
+		
+	}
+	public float deflection(float wofx) {
+		
+		
+		return wofx;
+	}
 }

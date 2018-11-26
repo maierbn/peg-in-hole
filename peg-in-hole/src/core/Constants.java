858ac8b32172eeba9ae513518bed58af6d4d5e47
matze <matze@matze-pc>
2018-11-26 14:49:38 +0100

    Created class Constants

---
+++ peg-in-hole/src/core/Constants.java
@@ -0,0 +1,8 @@
+package core;
+
+/*
+ * This class has some physical constants, that are independent of our FlexibleObject
+ */
+public class Constants {
+	static float gravitationalAcceleration = 9.81f;
+}

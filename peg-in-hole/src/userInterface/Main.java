d33d01e17a5de8c254e58a2c027a4670b44d7d5a
matze <matze@matze-pc>
2018-11-26 15:11:49 +0100

    Main.java cleanup

--- peg-in-hole/src/userInterface/Main.java
+++ peg-in-hole/src/userInterface/Main.java
@@ -6,24 +6,19 @@ import core.Formulas;
 public class Main {
 
 	public static void main(String[] args) {
-		FlexibleObject f = new FlexibleObject(0.2f, 0.05f, 0.02f, (float) (3.6f * Math.pow(10, 6)));
+		testDeflection();
+	}
+
+	public static void testDeflection() {
+		float length = 0.2f;
+		float width = 0.05f;
+		float heigth = 0.01f;
+		float youngsModulus = (float) (3.6f * Math.pow(10, 6));
+		
+		FlexibleObject f = new FlexibleObject(length, width, heigth, youngsModulus);
 		
 		for(float x = 0; x <= f.length; x += 0.01) {
 			System.out.println("w(" + x + ") = " + Formulas.deflection(f, x));
 		}
-
-	}
-
-	public float inertia(float I) {
-		float h = 0;		//height
-		float b = 0;		//width
-		I = (b*(h*h*h))/12;
-		return I;
-		
-	}
-	public float deflection(float x) {
-		
-		
-		return x;
 	}
 }

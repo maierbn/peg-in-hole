5d894d496076fe7be8a31c898786b9fa626d5cb9
kamikatze13 <kamikatze13@gmail.com>
2018-12-14 14:45:24 +0100

    added a GUI representation of the object deformation todo: comment in the log class references when it's been pushed

--- peg-in-hole/src/core/Simulation.java
+++ peg-in-hole/src/core/Simulation.java
@@ -1,20 +1,33 @@
 package core;
 
 public class Simulation {
-	public FlexibleObject f;
-	public int steps;
-	
+	public FlexibleObject flexObj;
+	public int deflectionRes;
+	public double[] deflectionValArray;
+
 	public Simulation(FlexibleObject f, int steps) {
-		this.f = f;
-		this.steps = steps;
+		this.flexObj = f;
+		this.deflectionRes = steps;
 	}
-	
-	public void start() {
-		Log.print("Starting Simulation..");
-		for(double x = 0; x <= f.length; x += (f.length / steps)) {
-			double deflection = Formulas.deflection(f, x);
-			Log.print("Deflection(x=" + x * 1000 + "mm) = " + deflection * 1000 + "mm");
+
+	public void deflectionValues(FlexibleObject object, int steps) {
+
+		deflectionValArray = new double[steps+1];
+
+		int i = 0;
+		for (double x = 0; x <= object.length; x += (object.length / steps)) {
+			deflectionValArray[i] = Formulas.deflection(object, x);
+			i++;
 		}
 	}
 
+	public void start() {
+//		Log.print("Starting Simulation..");
+		System.out.println("Calculatin deflection values...");
+		deflectionValues(this.flexObj, this.deflectionRes);
+		System.out.println("Done");
+		
+		
+//		Log.print("Deflection(x=" + x * 1000 + "mm) = " + deflection * 1000 + "mm");
+	}
 }
--- peg-in-hole/src/userInterface/Main.java
+++ peg-in-hole/src/userInterface/Main.java
@@ -1,7 +1,10 @@
 package userInterface;
 
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import org.knowm.xchart.*;
 import core.FlexibleObject;
-import core.Log;
+//import core.Log;
 import core.Simulation;
 
 public class Main {
@@ -11,19 +14,43 @@ public class Main {
 	}
 
 	public static void testDeflection() {
-		double length = 0.040;	//meter
-		double width = 0.015;	//meter
-		double heigth = 0.002;	//meter
+		double length = 0.040; // meter
+		double width = 0.015; // meter
+		double heigth = 0.002; // meter
 		double density = 1150;
 		double youngsModulus = 3.6E6;
-		
-		int simulationSteps = 3;
-		
-		FlexibleObject f = new FlexibleObject(length, width, heigth, density, youngsModulus);
-		
-		Simulation sim = new Simulation(f, simulationSteps);
-		
+
+		/**
+		 * on how many points we want a deflection to be calculated
+		 * 
+		 * also reflects the amount of rigid blocks for collision detection (this - 1)
+		 * as well as amount of joints (this - 1)
+		 */
+		int simDeflectionRes = 3;
+
+		FlexibleObject flexObj = new FlexibleObject(length, width, heigth, density, youngsModulus);
+
+		Simulation sim = new Simulation(flexObj, simDeflectionRes);
+
 		sim.start();
+
+		// generate x axis values through some esoteric streaming bullshit
+		//double[] xData = IntStream.rangeClosed(0, simDeflectionRes).mapToDouble((x) -> (double) x).toArray();
 		
+		// improvement: x axis should be length of object, not amount of steps
+		double[] xData = IntStream
+				.rangeClosed(0, simDeflectionRes)
+				.mapToDouble(x -> x*length/simDeflectionRes)
+				.toArray();
+		
+		// get the deflection values from simulation as graph y data
+		double[] yData = sim.deflectionValArray;
+		// cause it doesn't bend upwards *lennyface*
+		double[] yDataInverted = DoubleStream.of(yData).map(x -> -x).toArray();
+
+		// create chart and show it
+		XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yDataInverted);
+		new SwingWrapper(chart).displayChart();
+
 	}
 }

77dabebc0fcca56a5411b5814ab4acabc2952a74
kamikatze13 <kamikatze13+github@gmail.com>
2019-01-10 13:22:08 +0100

    trajectory formula concept finished. to be debugged.

--- peg-in-hole/src/core/Formulas.java
+++ peg-in-hole/src/core/Formulas.java
@@ -35,13 +35,51 @@ public class Formulas {
 		return (q * x * x * (6 * L * L - 4 * L * x + x * x)) / (24 * E * I) + additional;
 	}
 	
+	/**
+	 * factorial function limited for 0, 1 and 2
+	 * for internal use in bernstein polynomial calculations
+	 * 
+	 * @param i		the integer that should be fac'd
+	 * @return 		i! or 0 if input out of range
+	 */
+	private static double fac(int i) {
+		switch (i) {
+		case 0:
+			return 1;
+		case 1:
+			return 1;
+		case 2:
+			return 2;
+		default:
+			return 0;
+		}
+	}
+	
+	/**
+	 * solves the bernstein polynomial for hardcoded degree n=2
+	 * 
+	 * @param i	range 0..2
+	 * @param t	position on curve
+	 * @return	(n over i) * (1-t)^n-i * t^i
+	 */
 	private static double bernstein(int i, double t) {
 		// TODO write bernstein polynomial solution for n=2 hardcoded. 
 		// use pascal triangle LUT for efficient binomial coefficients
-		return 0;
+		// n=2 here, always
+		// i= 0..2
+		return (fac(2)/(fac(i)*fac(2-i)))*Math.pow(1-t, 2-i)*Math.pow(t, i);
 	}
+	
 	/**
 	 * calculate big B term, whatever that represents. some sort of weight
+	 * 
+	 * also, how the **** shall we divide a 3d vector by another 3d vector?
+	 * guess component-wise, again?
+	 * 
+	 * nope, divisor is a scalar - but still undefined?
+	 * just Point3D.multiply(1/divisor) it is then
+	 * 
+	 * multiply() and add() are already component-wise ops, should be fine
 	 * 
 	 * @param p0	staring point
 	 * @param cp	control point
@@ -51,8 +89,10 @@ public class Formulas {
 	 */
 	private static Point3D bigB(Point3D p0, Point3D cp, Point3D p1, double t) {
 		
-		// TODO write full formula
-		return p0.multiply(bernstein(0,t));
+		Point3D numerator = p0.multiply(bernstein(0,t)).add(cp.multiply(2*bernstein(1,t))).add(p1.multiply(bernstein(2, t)));
+		double divisor = bernstein(0,t)+2*bernstein(1,t)+bernstein(2,t);
+		
+		return numerator.multiply(1/divisor);
 	}
 	
 	/**
@@ -66,10 +106,11 @@ public class Formulas {
 	 */
 	public static Point3D trajectory(Point3D p0, Point3D cp, Point3D p1, double t) {
 		Point3D B = bigB(p0, cp, p1, t);
-		Point3D P0_neg = new Point3D(-p0.getX(), -p0.getY(), -p0.getZ());
-		Point3D P1minusP0 = p1.add(P0_neg);
+		//Point3D P0_neg = new Point3D(-p0.getX(), -p0.getY(), -p0.getZ());
+		Point3D P1minusP0 = p1.subtract(p0);
 		
-		// what is B*(P1-P0) supposed to do? crossproduct? dot product? coeff by coeff?
+		// what is B(t)*(P1-P0) supposed to do? crossproduct? dot product? coeff by coeff?
+		// if the latter, then:
 		double multCoeffX = B.getX()*P1minusP0.getX();
 		double multCoeffY = B.getY()*P1minusP0.getY();
 		double multCoeffZ = B.getZ()*P1minusP0.getZ();

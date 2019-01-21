896be327e484d9a42b7a5d280b83407d2fe0b1d2
kamikatze13 <kamikatze13+github@gmail.com>
2019-01-21 13:42:27 +0100

    trajectory drawing outsourced

---
+++ peg-in-hole/src/userInterface/TrajectoryDiagram.java
@@ -0,0 +1,46 @@
+package userInterface;
+
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
+import javafx.geometry.Point3D;
+
+public class TrajectoryDiagram {
+
+	public static void draw(int resolution, Point3D[] trajPoints) {
+		double[] xDataProj = new double[resolution];
+		double[] yDataProj = new double[resolution];
+		
+
+		for (int j = 0; j < xDataProj.length; j++) {
+			xDataProj[j] = trajPoints[j].getX();
+		}
+		
+
+		for (int j = 0; j < yDataProj.length; j++) {
+			yDataProj[j] = trajPoints[j].getY();
+		}
+		
+		
+//		// create chart and show it
+		XYChart chart = new XYChartBuilder().width(600).height(400).title("Trajectory").xAxisTitle("t").yAxisTitle("y").build();
+	
+		chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Line);
+		chart.getStyler().setChartTitleVisible(false);
+		chart.getStyler().setMarkerSize(8);
+		
+		XYSeries series = chart.addSeries("P(t)", xDataProj, yDataProj);
+		series.setMarker(SeriesMarkers.CIRCLE);
+			
+//		chart.getStyler().setXAxisMin(0d);
+//		chart.getStyler().setXAxisMax(1d);
+		
+		new SwingWrapper<XYChart>(chart).displayChart();
+	}
+	
+	
+}

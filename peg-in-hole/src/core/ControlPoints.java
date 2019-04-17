754b124ea6760c6c41cb1abb4e6b9d486e511a0b
matze <matze>
2019-04-17 23:03:11 +0200

    added stuff to controlpoints

--- peg-in-hole/src/core/ControlPoints.java
+++ peg-in-hole/src/core/ControlPoints.java
@@ -1,5 +1,27 @@
 package core;
 
+/**
+ * This class calculates and holds the controlpoints for a FlexibleObject
+ *
+ */
 public class ControlPoints {
-
+	public double[] x;
+	public double[] y;
+	public double[] z;
+	public double[] minDistance;
+	
+	FlexibleObject f;
+	
+	public ControlPoints(int count, FlexibleObject f){
+		x = new double[count];
+		y = new double[count];
+		z = new double[count];
+		minDistance = new double[count];
+		
+		f = this.f;
+	}
+	
+	void calculateXYZ() {
+		
+	}
 }

0be1126df9df23150d033ed766794e99d7fbede1
kamikatze13 <kamikatze13+github@gmail.com>
2019-05-01 11:56:53 +0200

    cosmetic update

--- peg-in-hole/src/examples/ExampleFunction.java
+++ peg-in-hole/src/examples/ExampleFunction.java
@@ -6,20 +6,19 @@ package examples;
 //sgpp.sparsegrids.org
 
 public class ExampleFunction extends sgpp.OptScalarFunction {
-public ExampleFunction() {
- super(2);
-}
+	public ExampleFunction() {
+		super(2);
+	}
 
-public double eval(sgpp.DataVector x) {
- if ((x.get(0) >= 0.0) && (x.get(0) <= 1.0) &&
-     (x.get(1) >= 0.0) && (x.get(1) <= 1.0)) {
-   // minimum is f(x) = -2 for x[0] = 3*pi/16, x[1] = 3*pi/14
-   return Math.sin(8.0 * x.get(0)) + Math.sin(7.0 * x.get(1));
- } else {
-   return Double.POSITIVE_INFINITY;
- }
-}
+	public double eval(sgpp.DataVector x) {
+		if ((x.get(0) >= 0.0) && (x.get(0) <= 1.0) && (x.get(1) >= 0.0) && (x.get(1) <= 1.0)) {
+			// minimum is f(x) = -2 for x[0] = 3*pi/16, x[1] = 3*pi/14
+			return Math.sin(8.0 * x.get(0)) + Math.sin(7.0 * x.get(1));
+		} else {
+			return Double.POSITIVE_INFINITY;
+		}
+	}
 
-public void clone(sgpp.SWIGTYPE_p_std__unique_ptrT_sgpp__optimization__ScalarFunction_t clone) {
-}
+	public void clone(sgpp.SWIGTYPE_p_std__unique_ptrT_sgpp__optimization__ScalarFunction_t clone) {
+	}
 }

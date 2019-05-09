55583ca6d48fb85a60bbe14005885ef281f887a6
kamikatze13 <kamikatze13+github@gmail.com>
2019-05-09 22:08:31 +0200

    second try: updated main to read from config file; generated jar, batch for easier execution

--- peg-in-hole/src/userInterface/Main.java
+++ peg-in-hole/src/userInterface/Main.java
@@ -1,5 +1,8 @@
 package userInterface;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import core.FlexibleObject;
 import core.Formulas;
 import core.Simulation;
@@ -7,40 +10,58 @@ import javafx.geometry.Point3D;
 
 public class Main {
 	public static void main(String[] args) {
-		double length = 0.080; // meter
-		double width = 0.015; // meter
-		double thickness = 0.002; // meter
-		double density = 1150;
-		double youngsModulus = 3.6E6;
+		String configFile = args[0];
+		Properties p = new Properties();
 
-		/**
-		 * for how many x-values do we want to calculate the deflection
-		 * 
-		 * also reflects the amount of rigid blocks for collision detection (this - 1)
-		 * as well as amount of joints (this - 1)
-		 */
-		int deflectionRes = 30;
-		int trajectoryRes = 30;
-		double slitSize = 0.005;
+		try {
+			p.load(new FileInputStream(configFile));
+			Boolean generateDatabase = Boolean.valueOf(p.getProperty("generateDatabase"));
+			Boolean simulateGivenControlPoint = Boolean.valueOf(p.getProperty("simulateGivenControlPoint"));
 
-		FlexibleObject f = new FlexibleObject(length, width, thickness, density, youngsModulus, deflectionRes);
+			double length = Double.parseDouble(p.getProperty("objectLength")); // meter
+			double width = Double.parseDouble(p.getProperty("objectWidth")); // meter
+			double thickness = Double.parseDouble(p.getProperty("objectThickness")); // meter
+			double density = Double.parseDouble(p.getProperty("objectDensity"));
+			double youngsModulus = Double.parseDouble(p.getProperty("objectYoungsModulus"));
 
-		// CONTROL POINTS
-		Point3D[] generatedCPs = Formulas.generateCPs(f, 40);
-		System.out.println("Generated a total of " + generatedCPs.length + " control points");
-		Formulas.generateSampleMatrix(generatedCPs, f, trajectoryRes, slitSize, 8, "results_successful.arff", true, true);
-		
-		
-		Point3D cp = new Point3D(2.97567275825991239824e-001*-1*0.1,
-				2.98237271554092494668e-010*0.1,
-				6.11962449156885246460e-001*20);
-		
-		// SIMULATION RUN
-		Simulation testSim = new Simulation(f, cp, trajectoryRes, slitSize);
-		testSim.calcTrajectory();
-		testSim.calcDeflectionsWithTrajectory();
-		double clearance = testSim.calcClearance();
-		System.out.println("Resulting clearance is " + clearance);
-		AnimatedDeflectionDiagram.draw(f, testSim.deflections, testSim.trajectory, testSim.slitSize);
+			/**
+			 * for how many x-values do we want to calculate the deflection
+			 * 
+			 * also reflects the amount of rigid blocks for collision detection (this - 1)
+			 * as well as amount of joints (this - 1)
+			 */
+			int deflectionRes = Integer.parseInt(p.getProperty("deflectionRes"));
+			int trajectoryRes = Integer.parseInt(p.getProperty("trajectoryRes"));
+			double slitSize = Double.parseDouble(p.getProperty("slitSize"));
+
+			FlexibleObject f = new FlexibleObject(length, width, thickness, density, youngsModulus, deflectionRes);
+
+			// CONTROL POINTS
+			if (generateDatabase) {
+				int amountOfSamples = Integer.parseInt(p.getProperty("amountOfSamples"));
+				Point3D[] generatedCPs = Formulas.generateCPs(f, amountOfSamples);
+				System.out.println("Generated a total of " + generatedCPs.length + " control points");
+				Formulas.generateSampleMatrix(generatedCPs, f, trajectoryRes, slitSize, 8, "results_successful.arff",
+						true, true);
+			}
+			// SIMULATION RUN
+
+			if (simulateGivenControlPoint) {
+				double cpX = Double.parseDouble(p.getProperty("cpX"));
+				double cpY = Double.parseDouble(p.getProperty("cpY"));
+				double cpZ = Double.parseDouble(p.getProperty("cpZ"));
+
+				Point3D cp = new Point3D(cpX * -1 * 0.1, cpY * 0.1, cpZ * 20);
+
+				Simulation testSim = new Simulation(f, cp, trajectoryRes, slitSize);
+				testSim.calcTrajectory();
+				testSim.calcDeflectionsWithTrajectory();
+				double clearance = testSim.calcClearance();
+				System.out.println("Resulting clearance is " + clearance);
+				AnimatedDeflectionDiagram.draw(f, testSim.deflections, testSim.trajectory, testSim.slitSize);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
